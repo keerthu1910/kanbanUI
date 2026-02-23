4c64c0364e13d6f6db26d93fa5ba09bf8586e9ef
keerthu1910 <keerthiselva1910@gmail.com>
2026-02-23 17:56:59 +0530

    Add kanban board ui along with card component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,23 @@
+export const Card = (props) => {
+  const { task, handleRight, handleLeft } = props;
+  return (
+    <div
+      key={task.id}
+      className="p-3 rounded-lg bg-gray-200 flex justify-between items-center m-2"
+    >
+      <p className="font-bold text-sm m-2">{task.taskname}</p>
+      <button
+        className="p-2 rounded-lg bg-white m-2 text-xs"
+        onClick={() => handleRight(task.id)}
+      >
+        Move Right
+      </button>
+      <button
+        className="p-2 rounded-lg bg-white text-xs"
+        onClick={() => handleLeft(task.id)}
+      >
+        Move Left
+      </button>
+    </div>
+  );
+};
---
+++ src/Components/Kanbanboard.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { Card } from "./Card";
+
+export const Kanbanboard = () => {
+  const [task, setTask] = useState({
+    id: "",
+    taskname: "",
+    status: "todo",
+  });
+  const [tasks, setTasks] = useState([]);
+  const handleTask = () => {
+    setTasks([...tasks, task]);
+    setTask({
+      id: "",
+      taskname: "",
+      status: "todo",
+    });
+  };
+  const handleRight = (taskid) => {
+    const temptasks = tasks.map((item) => {
+      if (item.id === taskid) {
+        if (item.status === "todo") {
+          return { ...item, status: "Inprocess" };
+        } else if (item.status === "Inprocess") {
+          return { ...item, status: "Completed" };
+        }
+      }
+      return item;
+    });
+    setTasks(temptasks);
+  };
+  const handleLeft = (taskid) => {
+    const temptasks = tasks.map((item) => {
+      if (item.id === taskid) {
+        if (item.status === "Inprocess") {
+          return { ...item, status: "todo" };
+        } else if (item.status === "Completed") {
+          return { ...item, status: "Inprocess" };
+        }
+      }
+      return item;
+    });
+    setTasks(temptasks);
+  };
+  return (
+    <div>
+      <p className="font-bold text-3xl text-center ">Kanban Board</p>
+      <div className="flex flex-row justify-center items-center">
+        <input
+          className="p-2 border rounded-lg m-2"
+          value={task.taskname}
+          onChange={(e) =>
+            setTask({
+              ...task,
+              id: Date.now().toString(),
+              taskname: e.target.value,
+            })
+          }
+        />
+        <button
+          className="p-2 rounded-lg bg-red-500 text-white"
+          onClick={handleTask}
+        >
+          Add Task
+        </button>
+      </div>
+      <div className="flex flex-row justify-evenly">
+        <div className="bg-gray-300 rounded-lg p-5 w-100 h-screen">
+          <p className="text-xl font-bold">Todo</p>
+          {tasks
+            .filter((item) => item.status === "todo")
+            .map((item) => (
+              <Card
+                task={item}
+                handleLeft={handleLeft}
+                handleRight={handleRight}
+              />
+            ))}
+        </div>
+        <div className="bg-gray-300 rounded-lg p-5 w-100 h-screen">
+          <p className="text-xl font-bold">In Progress</p>
+          {tasks
+            .filter((item) => item.status === "Inprocess")
+            .map((item) => (
+              <Card
+                task={item}
+                handleLeft={handleLeft}
+                handleRight={handleRight}
+              />
+            ))}
+        </div>
+        <div className="bg-gray-300 rounded-lg p-5 w-100 h-screen">
+          <p className="text-xl font-bold">Completed</p>
+          {tasks
+            .filter((item) => item.status === "Completed")
+            .map((item) => (
+              <Card
+                task={item}
+                handleLeft={handleLeft}
+                handleRight={handleRight}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
